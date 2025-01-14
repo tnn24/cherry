056f1bea4299d0bec884a7f687894f60129a32d1
Thien Nguyen <thnguyen24@gmail.com>
2025-01-13 19:03:01 -0800

    Added SpringDoc

---
+++ main-api/src/main/java/com/cherry/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.cherry.config;
+
+import com.cherry.constants.RESTPaths;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch(RESTPaths.SWAGGER_PATH_MATCH)
+                .build();
+    }
+}
--- main-api/src/main/java/com/cherry/constants/RESTPaths.java
+++ main-api/src/main/java/com/cherry/constants/RESTPaths.java
@@ -4,6 +4,8 @@ public class RESTPaths {
     public final static String BASE = "/api";
     public final static String ID = "/{id}";
 
+    public final static String SWAGGER_PATH_MATCH = BASE + "/**";
+
     public final static String ACCOUNTS = BASE + "/accounts";
     public final static String TRANSACTIONS = BASE + "/transactions";
 }
