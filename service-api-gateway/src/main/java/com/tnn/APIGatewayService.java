0f8e65d68c0e55aea400fe5b55caa87939d1b2f2
Thien Nguyen <thnguyen24@gmail.com>
2025-01-28 12:26:17 -0800

    Added API gateway service

---
+++ service-api-gateway/src/main/java/com/tnn/APIGatewayService.java
@@ -0,0 +1,11 @@
+package com.tnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGatewayService {
+    public static void main(String[] args) {
+        SpringApplication.run(APIGatewayService.class, args);
+    }
+}
