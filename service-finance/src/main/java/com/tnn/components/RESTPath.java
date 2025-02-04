d8315ae0616981c65a6f2b8733707a49316fb984
Thien Nguyen <thnguyen24@gmail.com>
2025-02-04 11:16:35 -0800

    Moved hard-coded strings to .env Updated api gateway

--- service-finance/src/main/java/com/tnn/components/RESTPath.java
+++ service-finance/src/main/java/com/tnn/components/RESTPath.java
@@ -1,9 +1,8 @@
 package com.tnn.components;
 
 public class RESTPath {
-    public final static String BASE = "/finance";
     public final static String ID = "/{id}";
 
-    public final static String ACCOUNTS = BASE + "/accounts";
-    public final static String TRANSACTIONS = BASE + "/transactions";
+    public final static String ACCOUNTS = "/accounts";
+    public final static String TRANSACTIONS = "/transactions";
 }
--- service-registration/src/main/java/com/tnn/component/RESTPath.java
+++ service-registration/src/main/java/com/tnn/component/RESTPath.java
@@ -1,9 +1,8 @@
 package com.tnn.component;
 
 public class RESTPath {
-    public final static String BASE = "/registration";
     public final static String ID = "/{id}";
 
-    public final static String FAMILY = BASE + "/families";
-    public final static String USER = BASE + "/users";
+    public final static String FAMILY = "/families";
+    public final static String USER = "/users";
 }
